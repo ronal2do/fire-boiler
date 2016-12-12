4830c9f1c069ecea21f4227d8cef0adec75008e9
Ronaldo <ronaldo@SOTMAC16.local>
2016-12-12 17:39:22 -0200

    front with auth

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
 
+import reducers from './reducers';
 import routes from './routes';
 
+import { AUTH_USER } from './actions/types';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// If we have a token, consider the user to be signed in
+if (token) {
+  // we need to update application state
+  store.dispatch({ type: AUTH_USER });
+}
+
 ReactDOM.render(
-  <Router history={hashHistory} routes={routes} />
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
   ,document.getElementById('root')
 );
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 
 import { scrollBefore } from './utils/routerInterceptor';
 
-import App from './App';
-import Home from './containers/Home';
+// Front
+import App from './containers/front/app';
+
+// Dashboard
+import Dashboard from './containers/dashboard/dashboard';
+import Signin from './containers/dashboard/auth/signin';
+import Signout from './containers/dashboard/auth/signout';
+import Signup from './containers/dashboard/auth/signup';
+import Feature from './containers/dashboard/feature';
+import RequireAuth from './containers/dashboard/auth/require_auth';
+import Welcome from './containers/dashboard/welcome';
 
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={Home} onEnter={scrollBefore} />
-  </Route>
+  <Router >
+    <Route path="/" component={App}>
+      <IndexRoute component={Welcome} onLeave={scrollBefore} />
+    </Route>
+    <Route path="/dashboard" component={Dashboard}>
+      <IndexRoute component={Welcome} />
+      <Route path="/" component={RequireAuth(Feature)} />
+    </Route>
+    <Route path="signin" component={Signin} />
+    <Route path="signout" component={Signout} />
+    <Route path="signup" component={Signup} />
+  </Router>
 );
